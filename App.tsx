c41b7e2df089b92791a80cf0ca13d35be6268967
mina abaraghi <minaabaraghi@gmail.com>
2025-04-12 01:54:19 +0330

    build

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-// App.tsx
+import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import DashboardScreen from './screens/DashboardScreen';
